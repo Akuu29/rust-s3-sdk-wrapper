a1713d92783a4bf4bbecec7d8e42692b6ee3b09a
AKUU <57586401+Akuu29@users.noreply.github.com>
2023-06-17 23:27:38 +0900

    Initial project setup

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8083));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello World"
+}
